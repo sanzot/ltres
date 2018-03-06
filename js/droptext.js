0c2d8d33fb5f841143708f13c95b08f7fe120a32
Santiago Román Ferro <santiagoromanferro@MacBook-Pro-de-Santiago.local>
2018-03-06 12:53:52 +0100

    +Moocs, + iconos

--- js/droptext.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $('#drop li a').each(function(){
-        $(this).click(function(){
-            $(this).siblings('div').slideToggle(300);            
-        });        
-    });                
-
-});
